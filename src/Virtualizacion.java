d0896c5f7662d22e25b851bd840ba3d19b2be378
santyago0 <santyago0515@gmail.com>
2022-10-21 22:29:40 -0500

    Creacion de nueva clase llamada Virtualizacion

--- src/Virtualizacion.java
+++ src/Virtualizacion.java
@@ -3,5 +3,8 @@ public class Virtualizacion {
         System.out.println("Temas de proyectos explicados en centro de convenciones");
         System.out.println("Creacion de videojuegos");
         System.out.println("Realidad virtual");
+        System.out.println("Filtros de instagram");
+        System.out.println("Club de realidad virtual");
+        System.out.println("Diseño de personajes");
     }
 }
